50c761ef20b215e6d3a9475892a931f5512e268c
fajarriskihidayat <fajarrh2002@gmail.com>
2024-06-25 21:33:53 +0700

    updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,7 @@ const carSizeRoute = require("./carSizeRoute");
 const usersRoute = require("./usersRoute");
 
 router.get("/", (req, res) => {
-  res.send("Welcome to api documentation car management dashboard");
+  res.send("Welcome to api documentation car management dashboard!!!");
 });
 router.use("/api/cars", carsRoute);
 router.use("/api/sizes", isAdminOrSuperAdmin, carSizeRoute);
