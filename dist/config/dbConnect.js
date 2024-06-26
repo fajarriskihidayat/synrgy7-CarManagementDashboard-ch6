c3ab8f53a691c1c5885d481331c949f150b21b6f
fajarriskihidayat <fajarrh2002@gmail.com>
2024-06-26 07:31:44 +0700

    updated port

--- dist/config/dbConnect.js
+++ dist/config/dbConnect.js
@@ -7,13 +7,12 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 const dbHost = process.env.DB_HOST;
-const sequelize = new sequelize_1.Sequelize({
-    database: dbName,
-    username: dbUser,
-    password: dbPassword,
+const dbPort = process.env.DB_PORT;
+const sequelize = new sequelize_1.Sequelize(dbName, dbUser, dbPassword, {
     host: dbHost,
     dialect: "postgres",
     dialectModule: require("pg"),
+    port: 5432,
     benchmark: true,
 });
 exports.default = sequelize;
--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -7,14 +7,13 @@ const dbUser = process.env.DB_USER as string;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME as string;
 const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
 
-const sequelize = new Sequelize({
-  database: dbName,
-  username: dbUser,
-  password: dbPassword,
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
   dialect: "postgres",
   dialectModule: require("pg"),
+  port: 5432,
   benchmark: true,
 });
 
