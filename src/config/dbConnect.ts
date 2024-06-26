e4870a42302674c3e295f183a0b4af943f78147f
fajarriskihidayat <fajarrh2002@gmail.com>
2024-06-26 09:07:53 +0700

    updated

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -1,8 +1,24 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+import { Sequelize } from "sequelize";
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dbUser = process.env.POSTGRES_USER as string;
+const dbPassword = process.env.POSTGRES_PASSWORD;
+const dbName = process.env.POSTGRES_DATABASE as string;
+const dbHost = process.env.POSTGRES_HOST;
+const dbPort = process.env.PORT as number | undefined;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  port: dbPort,
+  dialect: "postgres",
+  dialectModule: require("pg"),
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
 
-export default pool;
+export default sequelize;
