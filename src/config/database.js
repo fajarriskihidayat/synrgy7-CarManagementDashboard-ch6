9fe2ddd26e3a198935ee20b28365d2f98e5a504a
fajarriskihidayat <fajarrh2002@gmail.com>
2024-06-26 08:51:51 +0700

    updated

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,10 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     port: process.env.PORT,
     dialect: "postgres",
   },
