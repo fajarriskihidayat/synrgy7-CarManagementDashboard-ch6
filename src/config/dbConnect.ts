bf68646ee6f45913df0dad50dcd1b105ae7fe99e
fajarriskihidayat <fajarrh2002@gmail.com>
2024-06-28 22:02:19 +0700

    updated

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -3,10 +3,10 @@ import { Sequelize } from "sequelize";
 const dotenv = require("dotenv");
 dotenv.config();
 
-const dbUser = process.env.POSTGRES_USER_USER as string;
-const dbPassword = process.env.POSTGRES_USER_PASSWORD;
-const dbName = process.env.POSTGRES_USER_DATABASE as string;
-const dbHost = process.env.POSTGRES_USER_HOST;
+const dbUser = process.env.POSTGRES_USER as string;
+const dbPassword = process.env.POSTGRES_PASSWORD;
+const dbName = process.env.POSTGRES_DATABASE as string;
+const dbHost = process.env.POSTGRES_HOST;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
