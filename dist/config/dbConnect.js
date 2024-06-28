abe6b872d88871fda4b971d60a65de7932096988
fajarriskihidayat <fajarrh2002@gmail.com>
2024-06-28 22:03:29 +0700

    change dist

--- dist/config/dbConnect.js
+++ dist/config/dbConnect.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
-const dbUser = process.env.POSTGRES_USER_USER;
-const dbPassword = process.env.POSTGRES_USER_PASSWORD;
-const dbName = process.env.POSTGRES_USER_DATABASE;
-const dbHost = process.env.POSTGRES_USER_HOST;
+const dbUser = process.env.POSTGRES_USER;
+const dbPassword = process.env.POSTGRES_PASSWORD;
+const dbName = process.env.POSTGRES_DATABASE;
+const dbHost = process.env.POSTGRES_HOST;
 const sequelize = new sequelize_1.Sequelize(dbName, dbUser, dbPassword, {
     host: dbHost,
     dialect: "postgres",
