2f01941e9e84094fc965d1beb7a1e3fb56b6ec0f
fajarriskihidayat <fajarrh2002@gmail.com>
2024-06-28 21:31:55 +0700

    fix

--- dist/config/dbConnect.js
+++ dist/config/dbConnect.js
@@ -3,18 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
-const dbUser = process.env.POSTGRES_USER;
-const dbPassword = process.env.POSTGRES_PASSWORD;
-const dbName = process.env.POSTGRES_DATABASE;
-const dbHost = process.env.POSTGRES_HOST;
+const dbUser = process.env.POSTGRES_USER_USER;
+const dbPassword = process.env.POSTGRES_USER_PASSWORD;
+const dbName = process.env.POSTGRES_USER_DATABASE;
+const dbHost = process.env.POSTGRES_USER_HOST;
 const sequelize = new sequelize_1.Sequelize(dbName, dbUser, dbPassword, {
     host: dbHost,
     dialect: "postgres",
     dialectModule: require("pg"),
-    dialectOptions: {
-        ssl: {
-            require: true,
-        },
-    },
+    dialectOptions: { ssl: { require: true } },
 });
 exports.default = sequelize;
--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -3,20 +3,16 @@ import { Sequelize } from "sequelize";
 const dotenv = require("dotenv");
 dotenv.config();
 
-const dbUser = process.env.POSTGRES_USER as string;
-const dbPassword = process.env.POSTGRES_PASSWORD;
-const dbName = process.env.POSTGRES_DATABASE as string;
-const dbHost = process.env.POSTGRES_HOST;
+const dbUser = process.env.POSTGRES_USER_USER as string;
+const dbPassword = process.env.POSTGRES_USER_PASSWORD;
+const dbName = process.env.POSTGRES_USER_DATABASE as string;
+const dbHost = process.env.POSTGRES_USER_HOST;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
   dialect: "postgres",
   dialectModule: require("pg"),
-  dialectOptions: {
-    ssl: {
-      require: true,
-    },
-  },
+  dialectOptions: { ssl: { require: true } },
 });
 
 export default sequelize;
